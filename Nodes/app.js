f7f1a0450b29172fa44418a12bde6a28f7b5bc08
ramansrsta <ramansresta@gmail.com>
2019-07-04 22:33:10 +0545

    second

--- Nodes/app.js
+++ Nodes/app.js
@@ -1,8 +1,35 @@
-const http = require('http'); //require is needed for importing modules and files
+// const http = require('http'); //require is needed for importing modules and files//after writing app.listen we need not to write this as well
 // const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes');
-const server = http.createServer(routes);
-console.log('I am loving it');
+// const routes = require('./routes');
+// const server = http.createServer(routes);
+// console.log('I am loving it');
+const app = express();
+// app.use((req,res,next) => {
+//     console.log('in the middleware');
+//     next();//allows request to move to next middle ware
+// });
 
-server.listen(3000);
+//Admin route
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).sendFile(path.join(__dirname,'views','pagenotFound.html'));
+});
+
+
+// const server = http.createServer(app);
+// server.listen(3000);
+
+//instead of other code we can use just
+app.listen(3000);
