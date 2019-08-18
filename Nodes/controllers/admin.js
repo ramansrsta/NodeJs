465c9a5103d60ec5aab3704429673699db7ab426
ramansrsta <ramansresta@gmail.com>
2019-08-18 22:27:52 +0545

    upload

--- Nodes/controllers/admin.js
+++ Nodes/controllers/admin.js
@@ -10,9 +10,17 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
+  const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
+  if(!image){
+    return res.status(422).render('admin/edit-product',{
+      pageTitle: 'Add Product',
+      path: '/admin/add-product'
+    })
+  }
+
+  const imageUrl = image.path;
   const product = new Product({
     title: title,
     price: price,
@@ -57,7 +65,7 @@ exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
+  const image = req.file;
   const updatedDesc = req.body.description;
 
   Product.findById(prodId)
@@ -69,7 +77,11 @@ exports.postEditProduct = (req, res, next) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
+      if(image)
+      {
+        product.imageUrl = image.path;
+      }
+      
       return product.save()
         .then(result => {
         console.log('UPDATED PRODUCT!');
